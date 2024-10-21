e07f1eaa8c12006fa393f623b8d796dabbfea896
LogananthanD <156403339+LogananthanD@users.noreply.github.com>
2024-10-21 15:54:14 +0530

    Update routes.js

--- bikeservice/backend/route/routes.js
+++ bikeservice/backend/route/routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const routerr = express.Router()
-
+//Importing all the controller functions
 const { signup, signin, setService, statusUpdate, getAll } = require('../controller/controller')
-
+//Mapping the end point
 routerr.get('/',getAll)
 routerr.post('/register',signup)
 routerr.post('/login',signin)
 routerr.post('/service',setService)
 routerr.patch('/updateStatus',statusUpdate)
-
+//Export the router
 module.exports = routerr
